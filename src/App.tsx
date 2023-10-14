d6c319e931122037a6105cecf8b36c53e4476c69
Pablo Darde <pablodarde@gmail.com>
2023-10-14 15:43:12 -0300

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,6 @@ import { fetchData } from "./components/repositories/InMemoryRepository";
 import "./components/styles/global.css";
 import { FormType } from "./components/types/types";
 
-// const formData: FormType = [
-//   { inputtype: 'inputtext', required: true, label: 'name', errorMessage: 'The field name is required' },
-//   { inputtype: 'inputtext', required: true, label: 'role', errorMessage: 'The field role is required' },
-//   { inputtype: 'inputtext', required: true, label: 'password', errorMessage: 'The field password is required' },
-//   { inputtype: 'select', required: true, label: 'languages', options: ['JavaScript', 'Python'] },
-//   { inputtype: 'select', required: true, label: 'years of experience', options: ['one', 'two', 'three'] },
-// ]
-
 function App() {
   const [formData, setFormData] = useState<FormType>([]);
 
